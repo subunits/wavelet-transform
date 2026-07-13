5154930cd00b2dbffdd2eb8f6f7d49b46181f637
Michael Listrom <155396848+subunits@users.noreply.github.com>
2026-07-12 21:08:31 -0400

    Fix wavelet transform calculation in Wavelet.hpp

--- include/Wavelet.hpp
+++ include/Wavelet.hpp
@@ -14,8 +14,10 @@ public:
         std::vector<T> temp(n);
         size_t h = n / 2;
         for (size_t i = 0; i < h; ++i) {
+            // Approximation: Average of the pair normalized by sqrt(2)
             temp[i] = (data[2 * i] + data[2 * i + 1]) / std::sqrt(2.0);
-            temp[i + h] = (data[2 * i + 1] - data[2 * i]) / std::sqrt(2.0);
+            // Detail: Difference of the pair normalized by sqrt(2)
+            temp[i + h] = (data[2 * i] - data[2 * i + 1]) / std::sqrt(2.0);
         }
         data = temp;
     }
