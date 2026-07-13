1972d6958d29677a0bd257472e06f9027661774f
Michael Listrom <155396848+subunits@users.noreply.github.com>
2026-07-12 20:17:09 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "Wavelet.hpp"
 
+int main() {
+    std::vector<double> signal = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+    
+    // Apply Transform
+    Haar<double>::transform(signal);
+    
+    for (const auto& val : signal) {
+        std::cout << val << " ";
+    }
+    return 0;
+}
